9ae84b087e10170703d36ff6261affa8ee16c095
fran <728676@alu.murciaeduca.es>
2024-03-07 19:25:06 +0100

    Extracción de Agenda con implement, encapsulación de los atributos phone y name y introducimos el metodo initAgenda

--- ed04/src/main/java/org/example/Agenda.java
+++ ed04/src/main/java/org/example/Agenda.java
@@ -10,8 +10,8 @@ import java.util.List;
  * @since 2024
  * @author fran
  */
-public class Agenda {
-    private List<Contacto> contacts; // Lista de Contacto
+public class Agenda implements IAgenda {
+    private List<Persona> contacts; // Lista de Contacto
 
     /**
      *
@@ -28,7 +28,7 @@ public class Agenda {
      */
     public void addContact(String name, String phone) {
         boolean exists = false;
-        for (Contacto c : contacts) {
+        for (Persona c : contacts) {
             if (c.getName().equalsIgnoreCase(name)) {
                 exists = true;
                 c.getPhones().add(phone);
@@ -37,7 +37,7 @@ public class Agenda {
         }
 
         if (!exists) {
-            Contacto newContact = new Contacto(name, phone);
+            Persona newContact = new Persona(name, phone);
             contacts.add(newContact);
         }
     }
@@ -47,10 +47,10 @@ public class Agenda {
      * @param name
      */
     public void removeContact(String name) {
-        Iterator<Contacto> it = contacts.iterator();
+        Iterator<Persona> it = contacts.iterator();
 
         while (it.hasNext()) {
-            Contacto c = it.next();
+            Persona c = it.next();
 
             if (c.getName().equalsIgnoreCase(name)) {
                 it.remove();
@@ -65,7 +65,7 @@ public class Agenda {
      * @param newPhone
      */
     public void modifyPhoneNumber(String name, String oldPhone, String newPhone) {
-        for (Contacto c : contacts) {
+        for (Persona c : contacts) {
             if (c.getName().equalsIgnoreCase(name)) {
                 List<String> phones = c.getPhones();
 
@@ -82,7 +82,7 @@ public class Agenda {
      * Metodo get para obtener los contactos
      * @return
      */
-    public List<Contacto> getContacts() {
+    public List<Persona> getContacts() {
         return this.contacts;
     }
 }
--- ed04/src/main/java/org/example/Persona.java
+++ ed04/src/main/java/org/example/Persona.java
@@ -7,7 +7,7 @@ import java.util.List;
  * @author fran
  * @version 1.0
  */
-public class Contacto {
+public class Persona {
     private String name;
     private List<String> phones;
 
@@ -16,10 +16,10 @@ public class Contacto {
      * @param name
      * @param phone
      */
-    public Contacto(String name, String phone) {
-        this.name = name;
-        this.phones = new ArrayList<>();
-        this.phones.add(phone);
+    public Persona(String name, String phone) {
+        this.setName(name);
+        this.setPhones(new ArrayList<>());
+        this.getPhones().add(phone);
     }
 
     /**
@@ -36,5 +36,13 @@ public class Contacto {
      */
     public List<String> getPhones() {
         return this.phones;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhones(List<String> phones) {
+        this.phones = phones;
     }
 }
